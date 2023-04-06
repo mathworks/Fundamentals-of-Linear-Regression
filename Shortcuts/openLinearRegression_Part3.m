5a886c45bba13d6af114873c6ffd68a42bf2d166
hyoshino <hyoshino@mathworks.com>
2023-04-06 13:30:49 +0900

    Fix "Project" issue and gave a little change to README.md

---
+++ Shortcuts/openLinearRegression_Part3.m
@@ -0,0 +1,11 @@
+function openLinearRegression_Part3
+% Open file and cd into file directory. This script can be modified 
+% to do whatever you'd like when clicking the project shortcut,
+% e.g., loading data, running an app, or loading a doc page.
+
+prj = currentProject;
+
+% Delete this line if you want to remain in project root folder
+cd(fullfile(prj.RootFolder, ""));
+
+open("LinearRegression_Part3.mlx")
